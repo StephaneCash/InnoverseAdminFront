a1c070e02817f204ee6482c8033cb9e60d89ca15
StéphaneCash <kikonistephane@gmail.com>
2023-01-26 20:43:29 +0100

    hh

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import "./Login.css";
 import logo from "../../images/logo.png";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { baseUrl } from '../../bases/baseUrl';
 
@@ -16,8 +16,6 @@ const SignIn = () => {
 
     const maxAge = 3 * 24 * 60 * 60 * 1000;
 
-    const navigate = useNavigate();
-
     const handleLogin = (e) => {
         e.preventDefault();
         setBtnClic(true);
@@ -26,7 +24,7 @@ const SignIn = () => {
                 localStorage.setItem('token', resp.data.token);
                 document.cookie = `jwt=${resp.data.token}; max-age=${maxAge}`;
                 setBtnClic(false);
-                navigate('/dashboard');
+                window.location = "/dashboard";
             })
             .catch(err => {
                 console.log(err.response.data.errors);
